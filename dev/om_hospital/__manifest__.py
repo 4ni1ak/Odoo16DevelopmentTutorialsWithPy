7ebc3ad42b2bab4fe0bbf61a4b72f9db9761fee0
Anıl Akpınar <4ni1ak@gmail.com>
2023-12-12 22:24:52 +0300

    Added Hospital Patient Form and Tree Views.

--- dev/om_hospital/__manifest__.py
+++ dev/om_hospital/__manifest__.py
@@ -3,6 +3,7 @@
     'author':'Anil Akpinar',
     'website':'https://www.akpinar.dev',
     'data':[
+        'security/ir.model.access.csv',
         'views/menu.xml',
         'views/patient.xml',
     ]
